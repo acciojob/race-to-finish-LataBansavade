db299564d21a618e6d701a827226e55c101f1200
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-08-28 13:20:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+    // Array of promises
+   window.promises = [
+      new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), getRandomTime())),
+    ];
+
+    // Use Promise.any() to wait for the first promise to resolve
+    Promise.any(promises)
+      .then((result) => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.textContent = result;
+      })
+      .catch((error) => {
+        console.error('All promises rejected:', error);
+      });
+ 
